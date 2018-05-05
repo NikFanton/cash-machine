bf4dce686b02d34c3237a6ce829bbcdd4214cb55
Nikita Guchenko <guchenko.n@gmail.com>
2018-05-05 10:42:29 +0300

    add new method to Employee sercvice

--- src/main/java/ua/training/model/service/EmployeeService.java
+++ src/main/java/ua/training/model/service/EmployeeService.java
@@ -2,13 +2,20 @@ package ua.training.model.service;
 
 import ua.training.model.dao.factory.DAOFactory;
 import ua.training.model.dao.factory.JdbcDAOFactory;
+import ua.training.model.dao.util.CryptoUtil;
 import ua.training.model.entity.Employee;
 
 public class EmployeeService {
     private DAOFactory daoFactory = new JdbcDAOFactory();
 
     public boolean isEmployeeExist(String login, String pass) {
-        return getEmployee(login, pass) != null;
+        Employee employee = getEmployee(login);
+        boolean isCorrectPassword = CryptoUtil.checkPassword(pass, employee.getPassword());
+        return getEmployee(login) != null && isCorrectPassword;
+    }
+
+    public Employee getEmployee(String login) {
+        return daoFactory.getEmployeeDAO().getByLogin(login);
     }
 
     public Employee getEmployee(String login, String pass) {
