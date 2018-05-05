0b176ce252f36f35949d2b27621aee5da7d793a9
Nikita Guchenko <guchenko.n@gmail.com>
2018-05-05 10:41:13 +0300

    add new constants

--- src/main/java/ua/training/controller/constant/CommandNames.java
+++ src/main/java/ua/training/controller/constant/CommandNames.java
@@ -1,4 +1,8 @@
 package ua.training.controller.constant;
 
-public class CommandNames {
+public interface CommandNames {
+    String LOGOUT = "logout";
+    String CREATE_CHECK = "create-check";
+    String CHECK_LIST = "check-list";
+    String REPORT = "report";
 }
--- src/main/java/ua/training/controller/constant/Templates.java
+++ src/main/java/ua/training/controller/constant/Templates.java
@@ -1,4 +1,10 @@
 package ua.training.controller.constant;
 
 public interface Templates {
+   String LOGIN = "/view/login.jsp";
+   String CREATE_CHECK = "/view/create-check.jsp";
+   String CHECK_LIST = "/view/check-list.jsp";
+   String REPORT = "/view/report.jsp";
+
+   String REDIRECT = "redirect:";
 }
