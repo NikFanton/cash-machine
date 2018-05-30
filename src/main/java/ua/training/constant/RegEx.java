c7dd4e2ce8b0b5c59bffdc849b2c88391ca20231
Nikita Guchenko <guchenko.n@gmail.com>
2018-05-30 13:24:06 +0300

    created new regex

--- src/main/java/ua/training/constant/RegEx.java
+++ src/main/java/ua/training/constant/RegEx.java
@@ -2,4 +2,7 @@ package ua.training.constant;
 
 public interface RegEx {
     String REGEX_NAME = "[1-9a-zа-щьюяґіїє `'\"\\-]{1,20}";
+    String REGEX_PRICE = "^\\d+(\\.\\d{1,2})?$";
+    String REGEX_QUANTITY_FOR_COUNTABLE = "^\\d{1,6}$";
+    String REGEX_QUANTITY_FOR_UNCOUNTABLE = "^\\d+(\\.\\d{1,3})?$";
 }
