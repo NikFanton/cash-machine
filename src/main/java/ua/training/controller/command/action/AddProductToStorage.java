5ed2543792d1fad87853cf1915e9bbdd94121cf2
Nikita Guchenko <guchenko.n@gmail.com>
2018-05-30 13:26:10 +0300

    fixed problems with incorrect data entry

--- src/main/java/ua/training/controller/command/action/AddProductToStorage.java
+++ src/main/java/ua/training/controller/command/action/AddProductToStorage.java
@@ -1,9 +1,10 @@
 package ua.training.controller.command.action;
 
-import ua.training.controller.annotation.CommandWithLocation;
-import ua.training.controller.command.Command;
 import ua.training.constant.AttributeAndParameterNames;
 import ua.training.constant.Locations;
+import ua.training.constant.RegEx;
+import ua.training.controller.annotation.CommandWithLocation;
+import ua.training.controller.command.Command;
 import ua.training.model.entity.Product;
 import ua.training.model.entity.enums.ProductType;
 import ua.training.model.service.ProductService;
@@ -21,15 +22,20 @@ public class AddProductToStorage implements Command {
     @Override
     public String execute(HttpServletRequest request) {
         String name = request.getParameter(AttributeAndParameterNames.PRODUCT_NAME);
-        Double quantity = Double.valueOf(request.getParameter(AttributeAndParameterNames.PRODUCT_QUANTITY));
-        Double price = Double.valueOf(request.getParameter(AttributeAndParameterNames.PRODUCT_PRICE));
+        String quantity = request.getParameter(AttributeAndParameterNames.PRODUCT_QUANTITY);
+        String price = request.getParameter(AttributeAndParameterNames.PRODUCT_PRICE);
         ProductType productType = ProductType.valueOf(request.getParameter(AttributeAndParameterNames.PRODUCT_TYPE));
-        productService.addProduct(Product.builder()
-                .setName(name)
-                .setQuantity(quantity)
-                .setPrice(price)
-                .setProductType(productType)
-                .build());
+        if ((productType == ProductType.COUNTABLE) ? quantity.matches(RegEx.REGEX_QUANTITY_FOR_COUNTABLE)
+                                                   : quantity.matches(RegEx.REGEX_QUANTITY_FOR_UNCOUNTABLE)
+                && price.matches(RegEx.REGEX_PRICE)
+                && name.matches(RegEx.REGEX_NAME)) {
+            productService.addProduct(Product.builder()
+                    .setName(name)
+                    .setQuantity(Double.parseDouble(quantity))
+                    .setPrice(Double.valueOf(price) * 100)
+                    .setProductType(productType)
+                    .build());
+        }
         return Locations.REDIRECT + Locations.ADD_PRODUCT_TO_STORAGE_FORM;
     }
 }
